ce81734e1fc785ed4efe6f6a25fc9eb75100da79
Wanderrful <wanderrful@gmail.com>
2019-07-18 07:17:10 -0500

    Tweaked router-outlet again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { FrameDataComponent } from './frame-data/frame-data.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: "/home", pathMatch: "full" },
-  { path: "/home", component: FrameDataComponent },
+  { path: "home", component: FrameDataComponent },
   { path: '**', component: FrameDataComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-
-import { Kazuya } from "../data/frames/kazuya.data";
-import { CharacterFrameDataHeaders, CharacterFrameData } from 'src/data/framedata.interface';
-import { fn_sortFrameData } from "../data/sortFrameData";
-import { fn_applyFilter } from "../data/applyFilter";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'tekken7-framedata';
-  displayedColumns: string[] = Object.keys(CharacterFrameDataHeaders);
-  dataSource = new MatTableDataSource(Kazuya);
-  defaultFilterPredicateFn: ((data: CharacterFrameData, filter: string) => boolean);
-
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
-
-  ngOnInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.sortingDataAccessor = fn_sortFrameData;
-    this.defaultFilterPredicateFn = this.dataSource.filterPredicate;
-    this.dataSource.filterPredicate = fn_applyFilter(this.defaultFilterPredicateFn);
-  }
-
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  onSpecialCounterHitStatus(isChecked: boolean) {
-    const regex = /( \?specialCounterHitStatus\? )/;
-    if (!!this.dataSource.filter.match(regex)) {
-      this.dataSource.filter = this.dataSource.filter.replace(regex, "");
-    } else {
-      this.dataSource.filter += " ?specialCounterHitStatus? ";
-    }
-  }
-
-  onToggleWhiffPunishFilter(isChecked: boolean) {
-    const regex = /( \?whiffPunisher\? )/;
-    if (!!this.dataSource.filter.match(regex)) {
-      this.dataSource.filter = this.dataSource.filter.replace(regex, "");
-    } else {
-      this.dataSource.filter += " ?whiffPunisher? ";
-    }
-  }
 }
